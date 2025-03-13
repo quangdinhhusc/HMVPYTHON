2b7933586503ca1e6090f3bb90d28969e9be987b
hagiaquangdinh <21t1020303@husc.edu.vn>
2025-03-14 03:29:39 +0700

    Update SemisupervisedNN.py

--- SemisupervisedNN.py
+++ SemisupervisedNN.py
@@ -332,8 +332,7 @@ def run_PseudoLabelling_app():
                             ax.set_title('Phân phối số lượng dữ liệu trong tập train')
                             ax.set_xticks(unique_labels)
                             st.pyplot(fig)
-                            st.session_state.show_training_options = True
-                            
+                            st.session_state.show_training_options = True                      
                         
             else:
                 st.error("🚨 Dữ liệu chưa được nạp. Hãy đảm bảo `train_images`, `train_labels` và `test_images` đã được tải trước khi chạy.")
