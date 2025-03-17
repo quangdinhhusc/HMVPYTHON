9aa63bc3ab43ba72b0b6ff3a3687ca8e0f6eec9e
hagiaquangdinh <21t1020303@husc.edu.vn>
2025-03-17 15:00:30 +0700

    Update SemisupervisedNN.py

--- SemisupervisedNN.py
+++ SemisupervisedNN.py
@@ -63,6 +63,8 @@ def data_preparation():
     num_samples = st.slider("Chọn số lượng ảnh để huấn luyện:", 100, total_samples, 20000)
     num_samples = num_samples - 1
 
+    
+
     # Thanh kéo chọn tỷ lệ Train/Test
     test_size = st.slider("Chọn % dữ liệu Test", 10, 50, 20)
     train_size = 1
